d319ac4c692d8720fe2e68ed0c94829adaaf4703
William-Bowman-JCU <william.bowman@my.jcu.edu.au>
2024-09-28 19:41:17 +1000

    Refactored score.py, and added random scores

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -6,17 +6,6 @@ Pseudocode for temperature conversion
 MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
-
-def convert_celsius_to_fahrenheit():
-    celsius = float(input("Celsius: "))
-    fahrenheit = celsius * 9.0 / 5 + 32
-    print(f"Result: {fahrenheit:.2f} F")
-
-def convert_fahrenheit_to_celsius():
-    fahrenheit = float(input('Fahrenheit: '))
-    celsius = 5 / 9 * (fahrenheit - 32)
-    print(f"Result: {celsius:.2f} C")
-
 def main():
     print(MENU)
     choice = input(">>> ").upper()
@@ -30,5 +19,16 @@ def main():
         print(MENU)
         choice = input(">>> ").upper()
     print("Thank you.")
+
+def convert_celsius_to_fahrenheit():
+    celsius = float(input("Celsius: "))
+    fahrenheit = celsius * 9.0 / 5 + 32
+    print(f"Result: {fahrenheit:.2f} F")
+
+def convert_fahrenheit_to_celsius():
+    fahrenheit = float(input('Fahrenheit: '))
+    celsius = 5 / 9 * (fahrenheit - 32)
+    print(f"Result: {celsius:.2f} C")
+
 
 main()
