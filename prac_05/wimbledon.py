b5060d8e4349b249358c63910d493f95ac5b1df8
William-Bowman-JCU <william.bowman@my.jcu.edu.au>
2024-10-18 21:01:25 +1000

    Complete prac 5 wimbledon exercise

---
+++ prac_05/wimbledon.py
@@ -0,0 +1,32 @@
+FILENAME = 'wimbledon.csv'
+
+def main():
+    """Read CSV and print the details about the Wimbledon champions"""
+    records = get_data()
+    champ_to_wins, countries = filter_records(records)
+    display_data(champ_to_wins, countries)
+
+def filter_records(records):
+    """Make a set containing each country, and a dictionary of the winners"""
+    champ_to_wins = {}
+    countries = set()
+    for record in records:
+        countries.add(record[1])
+        champ = record[2]
+        champ_to_wins[champ] = champ_to_wins.get(champ, 0) + 1
+    return champ_to_wins,countries
+
+def get_data():
+    """Get data from the CSV and append it to a list of lists"""
+    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()
+        return [line.strip().split(',') for line in in_file]
+
+def display_data(champ_to_wins, countries):
+    """Print champions & countries"""
+    print('Wimble Champions:')
+    for champ in champ_to_wins:
+        print(f'{champ} {champ_to_wins[champ]}')
+    print(f'\nThese 12 countries have won Wimbledon:\n{', '.join(sorted(countries))}')
+
+main()
