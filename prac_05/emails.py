79e8c1a057b7b0cf243b9101dabf4bdf1e78b39b
William-Bowman-JCU <william.bowman@my.jcu.edu.au>
2024-10-18 18:13:34 +1000

    refactored emails.py

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -3,17 +3,24 @@ Emails
 Estimate: 5 minutes
 Actual:   5 minutes
 """
-
-email = input('Email: ')
-name_to_email = {}
-while email != '':
-    name = email.split('@')[0].replace('.', ' ').title()
-    edit_name = input(f'Is your name {name}? (Y/n) ').lower()
-    if edit_name not in ['', 'y']:
-        name = input('Name: ')
-    name_to_email[name] = email
+def main():
     email = input('Email: ')
+    name_to_email = {}
 
-print()
-for name, email in name_to_email.items():
-    print(f'{name} ({email})')
+    while email != '':
+        name = extract_name(email)
+        edit_name = input(f'Is your name {name}? (Y/n) ').lower()
+        if edit_name not in ['', 'y']:
+            name = input('Name: ')
+        name_to_email[name] = email
+        email = input('Email: ')
+    print()
+    for name, email in name_to_email.items():
+        print(f'{name} ({email})')
+    
+    
+def extract_name(email):
+    name = email.split('@')[0].replace('.', ' ').title()
+    return name
+
+main()
