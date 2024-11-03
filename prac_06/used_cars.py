9086ed0e3d508175ed4a7e564fee188565d598d8
William-Bowman-JCU <william.bowman@my.jcu.edu.au>
2024-11-03 18:06:24 +1000

    Added name parameter & str method to Car class  Added new car 'Limo' to used_cars.py & tested new method

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -4,7 +4,7 @@ Note that the import has a folder (module) in it.
 This is why we name our folders with no spaces or capitals, as valid module names.
 """
 
-from prac_06.car import Car
+from car import Car
 
 
 def main():
@@ -13,6 +13,14 @@ def main():
     my_car.drive(30)
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
+
+    print('-------')
+
+    limo = Car('limousine', 100)
+    limo.add_fuel(20)
+    print(f"{limo.name} has fuel: {limo.fuel}")
+    limo.drive(115)
+    print(limo)
 
 
 main()
