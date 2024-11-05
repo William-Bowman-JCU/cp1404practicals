a2825aaf2657313c680e23e46bfc8adedc387449
William-Bowman-JCU <william.bowman@my.jcu.edu.au>
2024-11-05 22:15:00 +1000

    Get csv data and use a for loop to print it

---
+++ prac_07/myguitars.py
@@ -0,0 +1,17 @@
+from guitar import Guitar
+
+def main():
+    with open('prac_07/guitars.csv', 'r') as in_file:
+        guitars = []
+        in_file.readline()
+        for line in in_file:
+            data = line.strip().split(',')
+            guitar = Guitar(data[0], int(data[1]), float(data[2]))
+            guitars.append(guitar)
+    for guitar in guitars:
+        print(guitar)
+main()
+
+
+
+
