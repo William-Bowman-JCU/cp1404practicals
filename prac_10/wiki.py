c845fe52baac11c37c8ef90804870573ac6e7299
William-Bowman-JCU <william.bowman@my.jcu.edu.au>
2024-12-01 17:06:43 +1000

    Change printing summary to only show specified amount of sentences

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,10 +1,14 @@
 import wikipedia
 
+MAX_SENTENCES = 3
 title = input('Enter page title: ').strip()
 while title != '':
     try:
         page = wikipedia.page(title, auto_suggest=False)
-        print(page.summary.split('\n')[0])
+        print(page.title)
+        # As to not make an unnecessary wikipedia.summary API call, this is how I limited the amount of sentences shown from the summary property. 
+        # The only weakness I could find is if there is a decimal number if the sentences
+        print('.'.join(page.summary.split('.')[:MAX_SENTENCES]) + '.')
         print(page.url)
     except wikipedia.exceptions.DisambiguationError as DisambiguationError:
         print('We need a more specific title. Try one of the following, or a new search:')
